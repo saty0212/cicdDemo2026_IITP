e6a417ad5ed8f07ac071ac6f59c95ab9ab44ac38
saty0212 <sschouhan.cse@mnit.ac.in>
2026-02-21 08:38:55 +0530

    initial project files

---
+++ myApp.java
@@ -0,0 +1,6 @@
+public class myApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+       System.out.println("Addition of 5 and 3: " + MathUtil.add(5, 3));
+    }
+}
